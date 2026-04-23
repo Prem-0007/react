f2372ffd18b3d0ac9b22948a5360f4c63a34a4bc
Prem <premkumarballa777@gmail.com>
2026-04-24 01:16:52 +0530

    Set base path for Vite configuration

--- lesss go/vite.config.js
+++ lesss go/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/react/lesss%20go/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
